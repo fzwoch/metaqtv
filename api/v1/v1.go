1c7e1c0759ae54040d99752e4a3ac987b1a9c612
Viktor Persson <viktor.persson@arcsin.se>
2022-05-20 16:49:22 +0200

    Update api v1 output.

--- api/v1/v1.go
+++ api/v1/v1.go
@@ -30,18 +30,20 @@ func GameStateFromMdvsv(mvdsv mvdsv.MvdsvExport) GameState {
 		players = append(players, Player{Name: p.Name.ToPlainString()})
 	}
 
-	port, _ := strconv.Atoi(strings.Split(mvdsv.Address, ":")[1])
+	addressParts := strings.Split(mvdsv.Address, ":")
+	ip := addressParts[0]
+	port, _ := strconv.Atoi(addressParts[1])
 
 	return GameState{
-		Hostname:  mvdsv.Address,
-		IpAddress: mvdsv.Address,
+		Hostname:  ip,
+		IpAddress: ip,
 		Port:      port,
 		Link:      fmt.Sprintf("http://%s/watch.qtv?sid=3", mvdsv.Address),
 		Players:   players,
 	}
 }
 
-func ToGameStates(servers []mvdsv.MvdsvExport) any {
+func ToGameStates(servers []mvdsv.MvdsvExport) []GameState {
 	states := make([]GameState, 0)
 
 	for _, s := range servers {
@@ -54,8 +56,47 @@ func ToGameStates(servers []mvdsv.MvdsvExport) any {
 }
 
 func ServersHandler(serverSource func() []mvdsv.MvdsvExport) http.HandlerFunc {
-	getGameStates := func() any { return ToGameStates(serverSource()) }
-	return mhttp.CreateHandler(getGameStates)
+	getOutput := func() any {
+		type serverStats struct {
+			ServerCount       int
+			ActiveServerCount int
+			PlayerCount       int
+			ObserverCount     int
+		}
+
+		servers := serverSource()
+
+		stats := serverStats{
+			ServerCount:       len(servers),
+			ActiveServerCount: 0,
+			PlayerCount:       0,
+			ObserverCount:     0,
+		}
+
+		for _, s := range servers {
+			if s.PlayerSlots.Used > 0 {
+				stats.ActiveServerCount++
+			}
+			stats.PlayerCount += s.PlayerSlots.Used
+			stats.ObserverCount += s.SpectatorSlots.Used
+		}
+
+		gameStates := ToGameStates(servers)
+
+		type server struct{ GameStates []GameState }
+		type result struct {
+			Servers []server
+			serverStats
+		}
+
+		return result{
+			Servers: []server{
+				{GameStates: gameStates},
+			},
+			serverStats: stats,
+		}
+	}
+	return mhttp.CreateHandler(getOutput)
 }
 
 func New(baseUrl string, provider *dataprovider.DataProvider) mhttp.Api {
